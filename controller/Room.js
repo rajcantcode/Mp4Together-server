1619c7bb55555951dcb091de2df3ef84b909a47d
rajcantcode <mahadeshwarraj23@gmail.com>
2024-05-07 16:23:09 +0530

    fix: remove n+1 queries, add redis pipeline

--- controller/Room.js
+++ controller/Room.js
@@ -5,7 +5,7 @@ import { Room } from "../model/Room.js";
 import { nanoid } from "nanoid";
 import axios, { AxiosError } from "axios";
 import redis from "../lib/databases/redis.js";
-import { roomToAdmin } from "../index.js";
+import { roomToAdmin, usernameToSocketId } from "../index.js";
 export const createRoom = async (req, res) => {
   try {
     // Get username from req, which is passed by middleware
@@ -19,7 +19,7 @@ export const createRoom = async (req, res) => {
     if (user.roomId !== "") {
       const room = await Room.findOne({ mainRoomId: user.roomId });
       if (room) {
-        await removeUserFromRoom(room.mainRoomId, user._id);
+        await removeUserFromRoom(room.mainRoomId, user.username);
       }
     }
 
@@ -29,27 +29,29 @@ export const createRoom = async (req, res) => {
     const newRoom = new Room({
       mainRoomId: roomId,
       socketRoomId,
-      members: [user._id],
-      admins: [user._id],
+      members: [user.username],
+      admins: [user.username],
       membersMicState: { [user.username]: false },
     });
     await newRoom.save();
     roomToAdmin[roomId] = [user.username];
 
     // Also save the roomId and room in User collection in DB
+
     user.roomId = roomId;
     user.room = newRoom._id;
     await user.save();
 
     // Cache room and user
     // Removing room property, since we are going to separately cache room anyways
+    const pipeline = redis.pipeline();
     const userToCache = {
       email: user.email,
       username: user.username,
       roomId: user.roomId,
       socketId: user.socketId,
     };
-    await redis.hset(`user:${user.username}`, userToCache);
+    pipeline.hset(`user:${user.username}`, userToCache);
 
     const roomToCache = {
       mainRoomId: newRoom.mainRoomId,
@@ -59,12 +61,13 @@ export const createRoom = async (req, res) => {
       admins: [user.username],
       membersMicState: newRoom.membersMicState,
     };
-    await redis.hset(`room:${newRoom.mainRoomId}`, {
+    pipeline.hset(`room:${newRoom.mainRoomId}`, {
       ...roomToCache,
       membersMicState: JSON.stringify(newRoom.membersMicState),
       members: JSON.stringify([user.username]),
       admins: JSON.stringify([user.username]),
     });
+    await pipeline.exec();
 
     res.status(200).json({
       roomId: user.roomId,
@@ -83,10 +86,7 @@ export const joinRoom = async (req, res) => {
   try {
     const mainRoomId = req.params.id;
     // Use Mongoose to find a room by roomId
-    const room = await Room.findOne({ mainRoomId })
-      .populate("members", "username")
-      .populate("admins", "username")
-      .exec();
+    const room = await Room.findOne({ mainRoomId });
     if (!room) {
       // If no room is found, return an error response
       console.log("Room not found");
@@ -102,7 +102,7 @@ export const joinRoom = async (req, res) => {
     if (user.roomId !== "" && user.roomId !== room.mainRoomId) {
       const room = await Room.findOne({ mainRoomId: user.roomId });
       if (room) {
-        await removeUserFromRoom(room.mainRoomId, user._id);
+        await removeUserFromRoom(room.mainRoomId, user.username);
       }
     }
 
@@ -114,19 +114,18 @@ export const joinRoom = async (req, res) => {
     // Cache user
     // Removing room property, since we are going to separately cache room anyways
     const { room: userRoom, ...userToCache } = user;
-    await redis.hset(`user:${user.username}`, "roomId", room.mainRoomId);
+    const pipeline = redis.pipeline();
+    pipeline.hset(`user:${user.username}`, "roomId", room.mainRoomId);
 
     // Check if the user is already present in room, if present return the response
-    const isUserInMembers = room.members.some(
-      (member) => member.username === username
-    );
+    const isUserInMembers = room.members.includes(username);
     if (isUserInMembers) {
       // User is already a member in room
       res.status(200).json({
         roomId: room.mainRoomId,
         socketRoomId: room.socketRoomId,
-        members: room.members.flatMap(({ username }) => username),
-        admins: room.admins.flatMap(({ username }) => username),
+        members: room.members,
+        admins: room.admins,
         username: user.username,
         email: user.email,
         videoUrl: room.videoUrl,
@@ -136,26 +135,21 @@ export const joinRoom = async (req, res) => {
     }
 
     // Update the members array of the room collection
-    room.members.push(user._id);
-    room.membersMicState[user.username] = false;
+    room.members.push(username);
+    room.membersMicState[username] = false;
     room.markModified("membersMicState");
-    await room.save();
-
-    const updatedRoom = await Room.findOne({ mainRoomId })
-      .populate("members", "username")
-      .populate("admins", "username")
-      .exec();
+    const updatedRoom = await room.save();
 
     const roomObjToSend = {
       roomId: updatedRoom.mainRoomId,
       socketRoomId: updatedRoom.socketRoomId,
-      members: updatedRoom.members.flatMap(({ username }) => username),
-      admins: updatedRoom.admins.flatMap(({ username }) => username),
+      members: updatedRoom.members,
+      admins: updatedRoom.admins,
       videoUrl: updatedRoom.videoUrl,
       membersMicState: updatedRoom.membersMicState,
     };
     // Cache room
-    await redis.hset(`room:${updatedRoom.mainRoomId}`, {
+    pipeline.hset(`room:${updatedRoom.mainRoomId}`, {
       mainRoomId: updatedRoom.mainRoomId,
       socketRoomId: updatedRoom.socketRoomId,
       videoUrl: updatedRoom.videoUrl,
@@ -163,6 +157,7 @@ export const joinRoom = async (req, res) => {
       admins: JSON.stringify(roomObjToSend.admins),
       membersMicState: JSON.stringify(roomObjToSend.membersMicState),
     });
+    await pipeline.exec();
     // Send roomId and socketRoomId in the response
     res.status(200).json({
       username: user.username,
@@ -182,17 +177,19 @@ export const exitRoom = async (req, res) => {
     // Remove the user from members array and admins array for the specific room
     const mainRoomId = req.params.id;
     const memberToRemove = req.user.name;
-    const user = await User.findOne({ username: memberToRemove });
-
     // If the operation is successful, the response msg to be sent to the user will be returned by "removeUserFromRoom" function
-    const msg = await removeUserFromRoom(mainRoomId, user._id, memberToRemove);
+    const msg = await removeUserFromRoom(mainRoomId, memberToRemove);
 
     // Set the roomId property of the user to ""
-    // const user = await User.findOne({ username: memberToRemove });
-    user.roomId = "";
-    user.room = null;
-    user.socketId = "";
-    await user.save();
+    const user = await User.findOneAndUpdate(
+      { username: memberToRemove },
+      {
+        roomId: "",
+        room: null,
+        socketId: "",
+      },
+      { new: true }
+    );
 
     // Cache user
     await redis.hdel(`user:${user.username}`, "roomId", "socketID");
@@ -219,14 +216,6 @@ export const saveUrl = async (req, res) => {
       return res.status(400).json({ msg: "No video url provided" });
     }
 
-    // Check if room exists
-    const room = await Room.findOne({ mainRoomId: roomId })
-      .populate("admins", "username")
-      .exec();
-    if (!room) {
-      return res.status(404).json({ msg: "No such room exists" });
-    }
-
     // Check if the sent url is valid
     const urlRegex = /^https:\/\/www\.youtube-nocookie\.com\/embed\/([^\/]+)$/;
     const match = videoUrl.match(urlRegex);
@@ -239,17 +228,19 @@ export const saveUrl = async (req, res) => {
         `https://www.youtube.com/oembed?format=json&url=https://www.youtube.com/watch?v=${videoId}`
       );
 
-      // If url is valid, verify if the user who sent request is an admin in the room
-      const admin = room.admins.find(
-        ({ username }) => username === req.user.name
+      // If url is valid, verify if the user who sent request is an admin in the room, then save the url
+
+      const room = await Room.findOneAndUpdate(
+        { mainRoomId: roomId, admins: { $in: [username] } },
+        { videoUrl: videoUrl },
+        { new: true }
       );
-      if (!admin) {
-        return res
-          .status(403)
-          .json({ msg: "Only admins are allowed to set video url" });
+
+      if (!room) {
+        return res.status(403).json({
+          msg: "Only admins are allowed to set video url or no such room exists",
+        });
       }
-      room.videoUrl = videoUrl;
-      await room.save();
 
       // Update room cache
       await redis.hset(`room:${room.mainRoomId}`, "videoUrl", videoUrl);
