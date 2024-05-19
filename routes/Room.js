1036dc4fe100800d16070214715774019e63fc4b
rajcantcode <mahadeshwarraj23@gmail.com>
2024-05-19 17:57:04 +0530

    feat: Add getVideoDetails route to Room.js

--- routes/Room.js
+++ routes/Room.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createRoom, joinRoom, exitRoom, saveUrl } from "../controller/Room.js";
+import {
+  createRoom,
+  joinRoom,
+  exitRoom,
+  saveUrl,
+  getVideoDetails,
+} from "../controller/Room.js";
 
 const router = express.Router();
 
@@ -7,6 +13,7 @@ router
   .post("/create", createRoom)
   .post("/join/:id", joinRoom)
   .post("/exit/:id", exitRoom)
+  .get("/youtube", getVideoDetails)
   .post("/:id", saveUrl);
 
 export default router;
